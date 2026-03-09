8e828395165547e0c748d862fbf99f1bd522b471
Alexandru Pavilcu <alexandrupavilcu@gmail.com>
2026-03-09 17:11:39 +0100

    lru_cache

---
+++ leetcode/quests/System and Software Design/Cache System Design Base/q1_lru_cache.py
@@ -0,0 +1,100 @@
+# Design a data structure that follows the constraints of a Least Recently Used (LRU) cache.
+#
+# Implement the LRUCache class:
+#
+# LRUCache(int capacity) Initialize the LRU cache with positive size capacity.
+# int get(int key) Return the value of the key if the key exists, otherwise return -1.
+# void put(int key, int value) Update the value of the key if the key exists. Otherwise, add the key-value pair to the cache. If the number of keys exceeds the capacity from this operation, evict the least recently used key.
+# The functions get and put must each run in O(1) average time complexity.
+
+
+# Example 1:
+#
+# Input
+# ["LRUCache", "put", "put", "get", "put", "get", "put", "get", "get", "get"]
+# [[2], [1, 1], [2, 2], [1], [3, 3], [2], [4, 4], [1], [3], [4]]
+# Output
+# [null, null, null, 1, null, -1, null, -1, 3, 4]
+#
+# Explanation
+# LRUCache lRUCache = new LRUCache(2);
+# lRUCache.put(1, 1); // cache is {1=1}
+# lRUCache.put(2, 2); // cache is {1=1, 2=2}
+# lRUCache.get(1);    // return 1
+# lRUCache.put(3, 3); // LRU key was 2, evicts key 2, cache is {1=1, 3=3}
+# lRUCache.get(2);    // returns -1 (not found)
+# lRUCache.put(4, 4); // LRU key was 1, evicts key 1, cache is {4=4, 3=3}
+# lRUCache.get(1);    // return -1 (not found)
+# lRUCache.get(3);    // return 3
+# lRUCache.get(4);    // return 4
+
+# Constraints:
+#
+# 1 <= capacity <= 3000
+# 0 <= key <= 104
+# 0 <= value <= 105
+# At most 2 * 105 calls will be made to get and put.
+
+from collections import OrderedDict
+
+class LRUCache:
+
+    def __init__(self, capacity: int):
+        if 1 <= capacity <= 3000:
+            self.cache = OrderedDict()
+            self.capacity = capacity
+            self.calls = 0
+
+
+    def get(self, key: int) -> int | None:
+        if self.calls < 2 * 10**5:
+            self.calls += 1
+            if key not in self.cache:
+                return -1
+
+            self.cache.move_to_end(key)
+            return self.cache[key]
+
+
+    def put(self, key: int, value: int) -> None:
+        if 0 <= key <= 10**4 and 0 <= value <= 10**5 and self.calls < 2 * 10**5:
+            self.calls += 1
+            if key in self.cache:
+                self.cache.move_to_end(key)
+            self.cache[key] = value
+
+            if len(self.cache) == self.capacity:
+                self.cache.popitem(last=False)
+
+
+# Your LRUCache object will be instantiated and called as such:
+# obj = LRUCache(capacity)
+# param_1 = obj.get(key)
+# obj.put(key,value)
+
+
+# ["LRUCache","put","put","get","put","get","put","get","get","get"]
+# [[2],[1,1],[2,2],[1],[3,3],[2],[4,4],[1],[3],[4]]
+# [[2],[2,1],[2,2],[2],[1,1],[4,1],[2]]
+# [[2],[2],[2,6],[1],[1,5],[1,2],[1],[2]]
+# [[2],[2,1],[3,2],[3],[2],[4,3],[2],[3],[4]]
+# ["LRUCache","put","put","put","put","put","get","put","get","get","put","get","put","put","put","get","put","get","get","get","get","put","put","get","get","get","put","put","get","put","get","put","get","get","get","put","put","put","get","put","get","get","put","put","get","put","put","put","put","get","put","put","get","put","put","get","put","put","put","put","put","get","put","put","get","put","get","get","get","put","get","get","put","put","put","put","get","put","put","put","put","get","get","get","put","put","put","get","put","put","put","get","put","put","put","get","get","get","put","put","put","put","get","put","put","put","put","put","put","put"]
+# [[10],[10,13],[3,17],[6,11],[10,5],[9,10],[13],[2,19],[2],[3],[5,25],[8],[9,22],[5,5],[1,30],[11],[9,12],[7],[5],[8],[9],[4,30],[9,3],[9],[10],[10],[6,14],[3,1],[3],[10,11],[8],[2,14],[1],[5],[4],[11,4],[12,24],[5,18],[13],[7,23],[8],[12],[3,27],[2,12],[5],[2,9],[13,4],[8,18],[1,7],[6],[9,29],[8,21],[5],[6,30],[1,12],[10],[4,15],[7,22],[11,26],[8,17],[9,29],[5],[3,4],[11,30],[12],[4,29],[3],[9],[6],[3,4],[1],[10],[3,29],[10,28],[1,20],[11,13],[3],[3,12],[3,8],[10,9],[3,26],[8],[7],[5],[13,17],[2,27],[11,15],[12],[9,19],[2,15],[3,16],[1],[12,17],[9,1],[6,19],[4],[5],[5],[8,1],[11,7],[5,2],[9,28],[1],[2,2],[7,4],[4,22],[7,24],[9,26],[13,28],[11,26]]
+
+if __name__ == "__main__":
+
+    commands = ["LRUCache","put","put","get","put","get","put","get","get","get"]
+    arguments = [[2],[1,1],[2,2],[1],[3,3],[2],[4,4],[1],[3],[4]]
+    # expected = [null,null,null,1,null,-1,null,-1,3,4]
+
+    capacity = arguments[0][0]
+    obj = LRUCache(capacity)
+
+    for i in range(1, len(commands)):
+        method_name = commands[i]
+        args = arguments[i]
+
+        method = getattr(obj, method_name)
+        print(method(*args))
+        ...
+
